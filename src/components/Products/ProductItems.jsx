881ac977b3644f558f512315a7d8b5cacc163b4b
Mishywayu <michellewayua2@gmail.com>
2022-09-16 22:49:22 +0300

    changes

--- src/components/Products/ProductItems.jsx
+++ src/components/Products/ProductItems.jsx
@@ -43,7 +43,7 @@ export default function Items() {
                         </Carousel>
                     </Col>
                     <Col>
-                        23
+                        
                     </Col>
                 </Row>
             </Container>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import Contact from '../Contact/Contact';
 import Navbar from '../Navbar/Navbar';
 import Items from './ProductItems';
 import './products.css';
 
 export default function Products() {
-    return(
+    return (
         <div>
             <div className='products'>
                 <div className='overlay'>
                     <header>
                         <Navbar />
                     </header>
+                    <h6 className='item-h6'>Straight into</h6>
+                    <h1 className='item-h1'>Style</h1>
+
+                    <p className="item-p">Fashion is the armor to survive the reality of everyday life.</p>
                 </div>
             </div>
             <Items />
+
+            <Contact />
         </div>
     );
 }
